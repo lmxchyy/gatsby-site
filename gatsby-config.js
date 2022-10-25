e8ba4ed0ea8bb968cd882802fe5dfbb5e3fa95bd
lmxchyy <lmxchyy@gmail.com>
2022-10-25 23:00:22 +0800

    gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,10 +16,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        spaceId:d5i1g7a6rolh,
-        accessToken:eDFar6QrLoq5Xf5z2GpoapD_CA4mhlyRfju66SlsQ8E
-        // spaceId: process.env.CONTENTFUL_SPACE_ID,
-        // accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
     },
     'gatsby-plugin-sass',
