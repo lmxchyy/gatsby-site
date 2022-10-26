adf493117601d94fd3e1b5390b76c441d2e5bc74
lmxchyy <lmxchyy@gmail.com>
2022-10-27 01:50:13 +0800

    modify env

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        spaceId:`d5i1g7a6rolh`,
-        // spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // spaceId:`d5i1g7a6rolh`,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
     },
